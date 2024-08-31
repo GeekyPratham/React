96c326fb2b86c3a26235e113e1f80a664fbc5777
GeekyPratham <kprathamraj2021@gmail.com>
2024-08-31 23:40:27 +0530

    first commit

---
+++ basic-react-app/src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css"
+
+function Title(){
+  return <h1>this is heading</h1>
+}
+
+function description(){
+  return <h1>I am description</h1>
+}
+
+function App() {
+ 
+  return(
+    <div>
+      <h1>hello pratham raj!</h1>
+      <p>welcome to my world</p>
+      <button>logIn</button>
+      <Title/>
+      <description/>
+    </div>
+  );
+}
+
+export default App
